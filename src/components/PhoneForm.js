93b97cea773457e3edad909e26d2ab1052d92ac2
luvrace <rambn1993@knou.ac.kr>
2021-02-22 20:43:27 +0900

    Add use input type

---
+++ src/components/PhoneForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class PhoneForm extends Component {
+    state = {
+        name:'',
+        phone:'',
+    }
+    handleChange = (e) => {
+        this.setState({
+        [e.target.name]: e.target.value
+    });
+
+    }
+    render() {
+        return (
+            <form>
+                <input 
+                    name="name"
+                    placeholder="이름"
+                    onChange={this.handleChange} 
+                    value={this.state.name}/>
+                <input 
+                    name="phone"
+                    placeholder="전화번호"
+                    onChange={this.handleChange}
+                    value={this.state.phone}
+                />
+                {this.state.name}
+                {this.state.phone}
+            </form>
+        )
+    }
+}
+
+export default PhoneForm;
