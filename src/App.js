790f38ae6aa97cbe71a61b4c11e749dc68ef6771
luvrace <rambn1993@knou.ac.kr>
2021-02-23 19:11:07 +0900

    Add delete update function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import Phone from './components/PhoneForm';
-function App() {
-  return (
-    <div>
-      <Phone />
-    </div>
-  );
+import React, { Component } from 'react'
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
+
+class App extends Component {
+  id = 0;
+
+  state = {
+    information:[],
+  }
+  handleCreate = (data)=>{
+    const { information } = this.state;
+    this.setState({
+      //비어있는 객체에 {} data를 넣고 id를 추가한다
+      information: information.concat(Object.assign({}, data, {
+        //1.
+        // ...data,
+        // id: this.id++
+
+        //2.
+        // name: data.name,
+        // phone: data.phone,
+        // id: this.id++
+
+        //3.
+        id: this.id++
+        
+      }))
+    })
+  }
+
+  handleRemove = (id) => {
+    const {information} = this.state;
+    this.setState({
+      infromation : information.filter(info => info.id !== id)
+    });
+  }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information : information.map(
+        info => {
+          if (info.id === id){
+          return {
+            id,
+            ...data,
+          };
+        }
+        return info;
+      }
+      )
+    })
+  }
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList 
+          data = {this.state.information }
+          onRemove = {this.handleRemove}
+          onUpdate={this.state.handleUpdate}/>
+      </div>
+    )
+  }
 }
 
-export default App;
 
 
+export default App
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -5,6 +5,18 @@ class PhoneForm extends Component {
         name:'',
         phone:'',
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        this.setState(
+            {
+                name:'',
+                phone:'',
+            }
+        )
+    }
+
     handleChange = (e) => {
         this.setState({
         [e.target.name]: e.target.value
@@ -13,20 +25,24 @@ class PhoneForm extends Component {
     }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input 
                     name="name"
                     placeholder="이름"
                     onChange={this.handleChange} 
-                    value={this.state.name}/>
+                    value={this.state.name}
+                    />
                 <input 
                     name="phone"
                     placeholder="전화번호"
                     onChange={this.handleChange}
                     value={this.state.phone}
                 />
-                {this.state.name}
-                {this.state.phone}
+                <button type="submit">등록</button>
+                <div>
+                    {this.state.name} {this.state.phone}
+                </div>
+                
             </form>
         )
     }
